5885a12aa310478161c4a23071e4488034a3ccc5
yd67 <yvens67@hotmail.fr>
2021-01-14 12:29:30 +0100

    bdd

--- includes/bdd.inc.php
+++ includes/bdd.inc.php
@@ -1,4 +1,11 @@
 <?php
 
+$host='localhost';
+$db='06-sql';
+$user='root';
+$password='';
+  
+  $bdd = new PDO('mysql:host=' .$host .';dbname=' . $db .';charset=utf8', $user ,$password );
+ // $bdd= new PDO('mysql:host=localhost;dbname-06sql;charset=utf8')
 
 ?>
